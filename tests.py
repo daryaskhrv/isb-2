df7506c17c0756a921f6950fa8abb91eb9923198
Darya Sakharova <daryasheqer@yandex.ru>
2023-03-30 18:24:19 +0400

    Second test

--- tests.py
+++ tests.py
@@ -13,7 +13,21 @@ def frequency_bit_test (sequence: list) -> float:
     return math.erfc(Sn/math.sqrt(2))
 
 
+def identical_bits_test(sequence: list) -> float:
+    N=len(sequence)
+    part1 = sequence.count(1)/N
+    if not abs(part1-0.5) < 2/math.sqrt(N):
+        return 0
+    Vn = 0
+    for i in range (0,N-1):
+        if sequence[i] != sequence[i+1]:
+            Vn += 1
+    return math.erfc((abs(Vn -2*N*part1*(1-part1)))/(2*math.sqrt(2*N)*part1*(1-part1)))
+
 
 if __name__ == '__main__':
-    p_1 = frequency_bit_test(s2)
-    print(len(s2), p_1)
+    #p_1 = frequency_bit_test(s2)
+    #print(len(s2), p_1)
+    identical_bits_test(s2)
+    '''for i in range (0,4):
+        print(i)'''
